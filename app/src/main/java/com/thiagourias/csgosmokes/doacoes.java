464a11619302ad02457991494451f146764f7d17
thg <thiagourias_@hotmail.com>
2019-01-06 03:22:32 -0200

    Finalização Página Donate

--- app/src/main/java/com/thiagourias/csgosmokes/doacoes.java
+++ app/src/main/java/com/thiagourias/csgosmokes/doacoes.java
@@ -1,13 +1,30 @@
 package com.thiagourias.csgosmokes;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class doacoes extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doacoes_app);
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void goToPaypal (View view) {
+        goToUrl ( "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YY99FWNJVPNMW");
+    }
+
+    public void goToPagseguro (View view) {
+        goToUrl ( "https://pag.ae/bcgnXyf");
     }
 }
